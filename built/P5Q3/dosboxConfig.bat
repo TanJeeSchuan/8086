1f43d2ee3365bb902ad7aeec5986bdbd28c09e85
TanJeeSchuan <89920999+TanJeeSchuan@users.noreply.github.com>
2023-09-02 14:02:50 +0800

    P5Q4

---
+++ built/P5Q3/dosboxConfig.bat
@@ -0,0 +1,10 @@
+[autoexec]
+MOUNT C C:\8086
+C:
+cls
+ECHO 8086 ASSEMBLY...
+masm.exe ASMFiles\P5\P5Q3.asm built\P5Q3\P5Q3 built\P5Q3\P5Q3 built\P5Q3\P5Q3;
+link.exe built\P5Q3\P5Q3.obj built\P5Q3\P5Q3;
+P5Q3.exe
+pause
+exit
---
+++ built/P5Q4/dosboxConfig.bat
@@ -0,0 +1,10 @@
+[autoexec]
+MOUNT C C:\8086
+C:
+cls
+ECHO 8086 ASSEMBLY...
+masm.exe ASMFiles\P5\P5Q4.asm built\P5Q4\P5Q4 built\P5Q4\P5Q4 built\P5Q4\P5Q4;
+link.exe built\P5Q4\P5Q4.obj built\P5Q4\P5Q4;
+P5Q4.exe
+pause
+exit
